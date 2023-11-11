c524dd20bc4b6bee0307bf585c1c45f3d73f1dbd
1000zoo <cjswldn99@gmail.com>
2023-11-12 00:52:53 +0900

    feat(MenuValidator): 단일 메뉴에대한 유효성 검증

---
+++ src/main/java/christmas/validator/MenuValidator.java
@@ -0,0 +1,25 @@
+package christmas.validator;
+
+import christmas.domain.Menu;
+
+public class MenuValidator implements Validator<String> {
+
+    private final static String INVALID_MENU_ORDERED_ERROR_MESSAGE = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
+
+    private final Menu menu;
+
+    public MenuValidator(Menu menu) {
+        this.menu = menu;
+    }
+
+    @Override
+    public void validate(String target) {
+        throwIfDoesNotExistMenu(target);
+    }
+
+    private void throwIfDoesNotExistMenu(String target) {
+        if (!menu.containsKey(target)) {
+            throw new IllegalArgumentException(INVALID_MENU_ORDERED_ERROR_MESSAGE);
+        }
+    }
+}
