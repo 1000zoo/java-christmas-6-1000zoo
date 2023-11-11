5c1e9816a9b1cd62800ea59829d7245fda22d78b
1000zoo <cjswldn99@gmail.com>
2023-11-11 22:42:53 +0900

    style(DateTest): 패키지 정리

--- src/test/java/christmas/vo/DateTest.java
+++ src/test/java/christmas/vo/DateTest.java
@@ -1,8 +1,7 @@
-package christmas;
+package christmas.vo;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import christmas.vo.Date;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
