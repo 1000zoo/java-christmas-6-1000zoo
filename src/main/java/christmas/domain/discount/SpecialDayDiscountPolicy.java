96f746c5cbc948cbd55589cd333ae52ff3171414
1000zoo <cjswldn99@gmail.com>
2023-11-12 15:51:47 +0900

    feat(SpecialDayDiscountPolicy): 특별할인 구현

---
+++ src/main/java/christmas/domain/discount/SpecialDayDiscountPolicy.java
@@ -0,0 +1,11 @@
+package christmas.domain.discount;
+
+public class SpecialDayDiscountPolicy implements SpecialEventPolicy {
+
+    private final static int SPECIAL_DISCOUNT_AMOUNT = 1000;
+
+    @Override
+    public int getDiscountAmount() {
+        return SPECIAL_DISCOUNT_AMOUNT;
+    }
+}
