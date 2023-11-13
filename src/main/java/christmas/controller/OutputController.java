8c49e4feb05a262b8e536c838af01565e6bbb8d6
1000zoo <cjswldn99@gmail.com>
2023-11-13 18:51:27 +0900

    feat(OutputController): 결과 출력 추가

--- src/main/java/christmas/controller/OutputController.java
+++ src/main/java/christmas/controller/OutputController.java
@@ -2,6 +2,9 @@ package christmas.controller;
 
 import christmas.constant.InstructionMessage;
 import christmas.constant.SummaryMessage;
+import christmas.domain.Badge;
+import christmas.domain.discount.EventPolicyType;
+import christmas.dto.BenefitResultDto;
 import christmas.dto.CustomerDto;
 import christmas.dto.OrderDto;
 import christmas.dto.OrdersDto;
@@ -13,6 +16,8 @@ public class OutputController {
     private final static String DATE_RESULT_FORMAT = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private final static String ORDER_RESULT_FORMAT = "%s %d개";
     private final static String KOREAN_WON_FORMAT = "#,###원";
+    private final static String EVENT_RESULTS_FORMAT = "%s: %s";
+    private final static String BENEFIT_KOREAN_WON_FORMAT = "-#,###원";
 
     private final OutputView outputView;
 
@@ -49,23 +54,43 @@ public class OutputController {
         outputView.printMessage(ORDER_RESULT_FORMAT, orderDTO.menuName(), orderDTO.quantity());
     }
 
-    public void printPolicyResult() {
-        printGiveawayEventResult();
-        printDiscountResult();
+    public void printPolicyResult(BenefitResultDto benefitResultDto) {
+        printGiveawayEventResult(benefitResultDto);
+        printEventList(benefitResultDto);
     }
 
-    private void printGiveawayEventResult() {
-        // 증정 메뉴 출력
+    private void printGiveawayEventResult(BenefitResultDto benefitResultDto) {
+        outputView.printMessage(SummaryMessage.GIVEAWAY_MENU.getMessage());
+        if (benefitResultDto.containsGiveaway()) {
+            outputView.printMessage("샴페인 1개");
+            return;
+        }
+        outputView.printMessage("없음");
     }
 
-    private void printDiscountResult() {
-        // 할인 내역 출력
-        // 총 혜택 금액 출력
-        // 할인 후 예상 결제 금액 출력
+    private void printEventList(BenefitResultDto benefitResultDto) {
+        outputView.printMessage(SummaryMessage.EVENT_RESULT.getMessage());
+        for (EventPolicyType eventPolicyType : EventPolicyType.values()) {
+            String typeName = eventPolicyType.getTypeName();
+            int benefitAmount = benefitResultDto.getBenefitOf(eventPolicyType);
+            outputView.printMessage(EVENT_RESULTS_FORMAT, typeName, benefitAmount);
+        }
     }
 
-    public void printBadgeResult() {
-        // 배지 결과 출력
+    public void printTotalBenefitAmount(int benefitAmount) {
+        DecimalFormat formatter = new DecimalFormat(BENEFIT_KOREAN_WON_FORMAT);
+        outputView.printMessage(SummaryMessage.BENEFITS_RESULT.getMessage());
+        outputView.printMessage(formatter.format(benefitAmount));
     }
 
+    public void printAfterDiscount(int amount) {
+        DecimalFormat formatter = new DecimalFormat(KOREAN_WON_FORMAT);
+        outputView.printMessage(SummaryMessage.AFTER_DISCOUNT.getMessage());
+        outputView.printMessage(formatter.format(amount));
+    }
+
+    public void printBadgeResult(Badge badge) {
+        outputView.printMessage(SummaryMessage.BADGE_RESULT.getMessage());
+        outputView.printMessage(badge.getName());
+    }
 }
