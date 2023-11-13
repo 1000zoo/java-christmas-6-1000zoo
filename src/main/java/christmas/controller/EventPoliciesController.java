aa689de8813aa014c1acb3bc1564e343ca727020
1000zoo <cjswldn99@gmail.com>
2023-11-13 16:33:32 +0900

    feat(EventPoliciesController): 중복 제거 준비단계

--- src/main/java/christmas/controller/EventPoliciesController.java
+++ src/main/java/christmas/controller/EventPoliciesController.java
@@ -3,9 +3,12 @@ package christmas.controller;
 import christmas.domain.Customer;
 import christmas.domain.discount.EventPolicies;
 import christmas.domain.discount.SpecialEventPolicy;
+import christmas.dto.PoliciesRequestDto;
 import christmas.vo.MenuInformation;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BooleanSupplier;
+import java.util.function.Function;
 
 public class EventPoliciesController {
 
@@ -14,12 +17,17 @@ public class EventPoliciesController {
     private final static int MIN_AMOUNT_FOR_GIVEAWAY = 120_000;
 
     private final Customer customer;
-    private final MenuInformation giveawayMenu;
+    private final PoliciesRequestDto policiesRequestDto;
     private final List<SpecialEventPolicy> policyList;
 
     public EventPoliciesController(Customer customer, MenuInformation giveawayMenu) {
         this.customer = customer;
-        this.giveawayMenu = giveawayMenu;
+        policiesRequestDto = new PoliciesRequestDto(
+                customer.countDessert(),
+                customer.countMain(),
+                customer.getDay(),
+                giveawayMenu.price()
+        );
         policyList = new ArrayList<>();
     }
 
@@ -34,10 +42,24 @@ public class EventPoliciesController {
         return EventPolicies.from(policyList);
     }
 
+    private void addPolicy(BooleanSupplier supplier, Function<PoliciesRequestDto, SpecialEventPolicy> constructor) {
+        if (supplier.getAsBoolean()) {
+            policyList.add(constructor.apply(policiesRequestDto));
+        }
+    }
+
+    private boolean canAddDDayDiscountPolicy() {
+        return customer.orderBeforeChristmas();
+    }
+
     private void addDDayDiscountPolicy() {
         if (customer.orderBeforeChristmas()) {
 //            policyList.add(new DDayDiscountPolicy(customer.getDay()));
         }
+    }
+
+    private boolean canAddWeekdayDiscountPolicy() {
+        return customer.orderAtWeekday() && customer.countDessert() > DOESNT_EXIST;
     }
 
     private void addWeekdayDiscountPolicy() {
@@ -46,16 +68,28 @@ public class EventPoliciesController {
         }
     }
 
+    private boolean canAddWeekendDiscountPolicy() {
+        return customer.orderAtWeekend() && customer.countMain() > DOESNT_EXIST;
+    }
+
     private void addWeekendDiscountPolicy() {
         if (customer.orderAtWeekend() && customer.countMain() > DOESNT_EXIST) {
 //            policyList.add(new WeekendDiscountPolicy(customer.countMain()));
         }
     }
 
+    private boolean canAddSpecialDayDiscountPolicy() {
+        return customer.orderAtSpecialDay();
+    }
+
     private void addDiscountAmount() {
         if (customer.orderAtSpecialDay()) {
 //            policyList.add(new SpecialDayDiscountPolicy());
         }
+    }
+
+    private boolean canAddGiveawayEventPolicy() {
+        return customer.calculateTotalPrice() >= MIN_AMOUNT_FOR_GIVEAWAY;
     }
 
     private void addGiveawayEventPolicy() {
