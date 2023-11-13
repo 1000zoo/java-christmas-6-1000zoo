7214ef0b864ec54ea2e4fbb865850d82820fb1fd
1000zoo <cjswldn99@gmail.com>
2023-11-13 17:00:22 +0900

    feat(EventPolicyType): 이벤트 정책 종류 enum

---
+++ src/main/java/christmas/domain/discount/EventPolicyType.java
@@ -0,0 +1,19 @@
+package christmas.domain.discount;
+
+public enum EventPolicyType {
+    DDAY("크리스마스 디데이 할인"),
+    WEEKDAY("평일 할인"),
+    WEEKEND("주말 할인"),
+    SPECIAL_DAY("특별 할인"),
+    GIVEAWAY("증정 이벤트");
+
+    private final String typeName;
+
+    EventPolicyType(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+}
