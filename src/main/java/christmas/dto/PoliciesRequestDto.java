ef0b49cdcd40812ec501a45bd2e9a0630d7271ba
1000zoo <cjswldn99@gmail.com>
2023-11-13 16:14:48 +0900

    fix(PoliciesRequestDto): 필드 변경
    - 증정 메뉴의 전체 객체는 필요 하지 않으므로
    - 증정 메뉴의 가격만 전달하도록 수정

--- src/main/java/christmas/dto/PoliciesRequestDto.java
+++ src/main/java/christmas/dto/PoliciesRequestDto.java
@@ -1,11 +1,9 @@
 package christmas.dto;
 
-import christmas.vo.MenuInformation;
-
 public record PoliciesRequestDto(
         int countDessert,
         int countMain,
         int day,
-        MenuInformation giveawayMenu
+        int giveawayMenuPrice
 ) {
 }
