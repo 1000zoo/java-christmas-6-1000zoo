9c86ccc0656b8ff25b9f324a946fee2e825f3f78
1000zoo <cjswldn99@gmail.com>
2023-11-15 04:31:02 +0900

    test(Controller): 입력에 따른 에러문구 테스트

--- src/test/java/christmas/controller/ControllerTest.java
+++ src/test/java/christmas/controller/ControllerTest.java
@@ -12,8 +12,8 @@ class ControllerTest extends NsTest {
 
     @ParameterizedTest
     @DisplayName("날짜 예외: 유효한 정수 입력이 아닐 경우")
-    @ValueSource(strings = {"1234567891011", "0", "-1", "32"})
-    void integerButInvalidDate(String input) {
+    @ValueSource(strings = {"1234567891011", "0", "-1", "32", "\s", "a", "가", "🎅🏻"})
+    void invalidDateInput(String input) {
         assertSimpleTest(() -> {
             runException(input);
             assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
@@ -21,9 +21,16 @@ class ControllerTest extends NsTest {
     }
 
     @ParameterizedTest
-    @DisplayName("날짜 예외: 입력이 정수가 아닐 경우")
-    @ValueSource(strings = {"\s", "a", "가", "🎅🏻"})
-    void noIntegerDate(String input) {
+    @DisplayName("주문 예외: 올바른 형식이 아닐 때")
+    @ValueSource(strings = {
+            "티라미수-3",   // 메뉴판에 없는 경우
+            "제로콜라-10",  // 음료만 시킨 경우
+            "타파스-1,양송이수프-10,타파스-1", // 중복 메뉴가 있는 경우
+            "크리스마스파스-0", "타파스-1,제로콜라-0,티본스테이크-2",   // 0이 포함된 경우
+            "아이스크림--", "바비큐립-1-", "티본스테이크-3,,", "타파스-31",
+            "해산물파스타-1,티본스테이크-2,"    // 주문 형식이 다른 경우
+    })
+    void invalidOrdersInput(String input) {
         assertSimpleTest(() -> {
             runException(input);
             assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
